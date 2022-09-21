06c72e3ce0368dbbf1b1779453f25aae15d551ce
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2022-09-21 17:05:10 -0300

    Adicionei solução ao requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (aName) => {
+  let student = {
+    name: aName,
+  };
+  student.feedback = () => 'Eita pessoa boa!';
+  return student;
+};
 
 module.exports = createStudent;
